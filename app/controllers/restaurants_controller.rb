1ec4b93d9e3d8e4df0c41e31fe145764bb7615bf
Eddie Kao <eddie@5xruby.tw>
2021-03-29 17:27:29 +0800

    add restaruant new page

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,4 +1,14 @@
 class RestaurantsController < ApplicationController
   def index
   end
+
+  def new
+  end
+
+  def list
+    # 寫入資料庫
+    # redirect -> 列表頁
+    # render html: "title is #{params[:title]}"
+    redirect_to '/restaurants'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   get '/restaurants', to: 'restaurants#index'
+
+  get '/new_restaurant', to: 'restaurants#new'
+
+  post '/restaurant_list', to: 'restaurants#list'
 end
