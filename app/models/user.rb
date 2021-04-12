35a69a6bda4779fbd4f4a56b79666fe8faaf8851
Eddie Kao <eddie@5xruby.tw>
2021-04-12 16:23:45 +0800

    WIP - user password encrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,24 @@
+require 'digest'
+
 class User < ApplicationRecord
-  attr_writer :password_confirmation
+  validates :email, presence: true,
+                    uniqueness: true,
+                    format: { with: /.+\@.+\..+/ }
+
+  validates :password, presence: true,
+                       confirmation: true
+
+  before_create :encrypt_password
+
+  private
+  def encrypt_password
+    self.password = Digest::SHA1.hexdigest(salted_pwd)
+    # self.password = Digest::SHA1.hexdigest(password)
+    # self.password=(Digest::SHA1.hexdigest(password))
+    # password = Digest::SHA1.hexdigest(password)
+  end
+
+  def salted_pwd
+    "123#{self.password}xx"
+  end
 end
