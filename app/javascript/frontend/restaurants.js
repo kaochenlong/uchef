d01473962f211a48bd6762daf688daa977a7f37e
Eddie Kao <eddie@digik.com.tw>
2021-04-26 16:54:24 +0800

    replace axios with rails/usj

--- app/javascript/frontend/restaurants.js
+++ app/javascript/frontend/restaurants.js
@@ -1,32 +1,27 @@
-import axios from 'axios'
+import Rails from "@rails/ujs"
 
 document.addEventListener('DOMContentLoaded', () => {
   let likeBtn = document.querySelector('#like-btn')
 
   if (likeBtn) {
-    let ax = axios.create()
-    let token = document.querySelector('meta[name=csrf-token]').content
-    ax.defaults.headers.common['X-CSRF-Token'] = token
-
     likeBtn.addEventListener('click', e => {
       let btn = e.target
       let id = btn.dataset['id']
 
-      ax.post(`/restaurants/${id}/pocket_list`)
-        .then(res => {
-          if (res.data.status == 'added') {
+      Rails.ajax({
+        url: `/restaurants/${id}/pocket_list`,
+        type: 'post',
+        success: res => {
+          if (res.status == 'added') {
             btn.classList.add('like-btn-favorited')
           } else {
             btn.classList.remove('like-btn-favorited')
           }
-        })
-        .catch(err => {
+        },
+        error: err => {
           console.log(err)
-        })
-
-      // console.log(123);
-      // POST ->
-      // POST /restaurants/2/pocket_list
+        }
+      })
     })
   }
 })
